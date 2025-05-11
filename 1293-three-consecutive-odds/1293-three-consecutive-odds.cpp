355a41866b74ce36aac7fd56729539d950f3493c
Tejas Giram <162475058+techgiram@users.noreply.github.com>
2025-05-11 23:28:50 +0530

    Time: 0 ms (100%), Space: 11.7 MB (98.85%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -0,0 +1,22 @@
+//(Iterating checking count of odd elements)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        int oddCount = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] % 2 == 1) {
+                oddCount++;
+            } else {
+                oddCount = 0;
+            }
+
+            if(oddCount == 3)
+                return true;
+        }
+        return false;
+    }
+};
